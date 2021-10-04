ff36a0b3113a413082235ce4824b552768d3b276
FabioCampaner <fabiocsuzuki@gmail.com>
2021-10-04 12:41:56 -0300

    Consumo de cadastro e login

--- NutriCommunity/src/app/app.module.ts
+++ NutriCommunity/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
@@ -16,6 +18,8 @@ import { ComoFuncionaComponent } from './como-funciona/como-funciona.component';
 import { HomeComponent } from './home/home.component';
 import { OngsComponent } from './ongs/ongs.component';
 import { ProdutosComponent } from './produtos/produtos.component';
+
+
 
 
 @NgModule({
@@ -33,14 +37,20 @@ import { ProdutosComponent } from './produtos/produtos.component';
     ComoFuncionaComponent,
     HomeComponent,
     OngsComponent,
-    ProdutosComponent
+    ProdutosComponent,
+   
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
