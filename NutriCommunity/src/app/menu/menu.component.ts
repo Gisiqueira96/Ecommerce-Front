7ae00931b59a97837dc1a6a61a3496a7c04c060b
FabioCampaner <fabiocsuzuki@gmail.com>
2021-10-14 21:08:47 -0300

    Ajuste do menu no modo escuro

--- NutriCommunity/src/app/menu/menu.component.ts
+++ NutriCommunity/src/app/menu/menu.component.ts
@@ -9,32 +9,44 @@ import { AuthService } from '../service/auth.service';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-nome = (environment.nome.toUpperCase().split(' ').slice(0, 1))
-homepage: boolean
+  nome = (environment.nome.toUpperCase().split(' ').slice(0, 1))
+  homepage: boolean
+
   constructor(
     public authService: AuthService,
     private router: Router
-    
   ) { }
 
   ngOnInit(
-    
+
   ) {
   }
-  sair(){
+  sair() {
     this.router.navigate(['/login'])
     environment.token = ''
     environment.nome = ''
     environment.id = 0
   }
-inicial(){
- 
-  if(this.router.url == "/home"){
-    return this.homepage = true;
-  }
-  else{
-    return this.homepage = false;
-  }
+  inicial() {
 
-}
+    if (this.router.url == "/home") {
+      return this.homepage = true;
+    }
+    else {
+      return this.homepage = false;
+    }
+
+
+  }
+  darkMode() {
+    const mode = document.querySelector('html')
+    if (mode?.classList.value == 'dark-mode') {
+      return true
+    }
+      return false    
+  }
+  changeMode() {
+    const mode = document.querySelector('html')
+    mode?.classList.toggle('dark-mode')
+  }
 }
